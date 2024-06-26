43ec2a809a14b95ac1a574ae950d28ec778752f8
Benjamin Ray <benjamin.ray@sky-real.com>
2024-06-26 15:33:48 +0200

    Add SkyRealPluginsToIgnore to required params to avoid compilation issues when a SkyReal plugin doesn't compile

--- scripts/Setup-SymLinks.ps1
+++ scripts/Setup-SymLinks.ps1
@@ -3,12 +3,18 @@ $Variables = & (Join-Path $PSScriptRoot Get-Variables.ps1)
 $InputUnrealProjectFilePath = $Variables.InputUnrealProject
 $InputUnrealProjectDirectoryPath = (Get-Item $InputUnrealProjectFilePath).Directory
 $RessourcesPluginsPath = $Variables.PluginDownloadDir
+$SkyRealPluginsToIgnore = $Variables.SkyRealPluginsToIgnore
 
 $SymLinks = @{
 	[IO.Path]::Combine($InputUnrealProjectDirectoryPath, "Content", "MetaHumans") = [IO.Path]::Combine($RessourcesPluginsPath, "Content", "MetaHumans");
 }
 
 Get-ChildItem -Path $RessourcesPluginsPath -Recurse -Filter "*.uplugin" | ForEach-Object {
+    if ($SkyRealPluginsToIgnore -contains $_.BaseName) 
+	{
+		Write-Host "Ignoring plugin " + $_.BaseName
+		Continue
+	}
 	$fi_info = Get-Item -Path $_.FullName
 	$SymLinks[[IO.Path]::Combine($InputUnrealProjectDirectoryPath, "Plugins", $fi_info.Directory.Name)] = $fi_info.Directory.FullName
     
