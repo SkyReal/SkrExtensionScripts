deae519e9918abd730567bf2aa62805290f63f47
Malek FARDEAU-SPIRE <38687753+M003T@users.noreply.github.com>
2025-07-02 17:53:44 +0200

    Revert local manifest changes after Build-Repository

--- scripts/Build-Repository.ps1
+++ scripts/Build-Repository.ps1
@@ -3,3 +3,6 @@
 
 & (Join-Path $PSScriptRoot Build-UEExtension.ps1)
 & (Join-Path $PSScriptRoot Build-Installer.ps1)
+
+# Revert any manifest changes (Editor)
+& (Join-Path $PSScriptRoot 'Create-Update-AllManifests.ps1') -ForceUpdate $true
